e15562fccd79da9ccfcb229b55a3c8314d189ff7
Ashraf Ali <ali4282271@gmail.com>
2023-12-15 23:34:47 -0800

    add animation

--- src/app/components/animation/slider.tsx
+++ src/app/components/animation/slider.tsx
@@ -1,49 +1,80 @@
 "use client"
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-
-type Image = {
-  images: string;
+import * as React from "react";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { wrap } from "popmotion";
+import { images } from "./image-data";
+import styles from "./slider.module.css";
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0
+    };
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0
+    };
+  }
 };
 
-const images: Image[] = [
-  { images: "flower1.avif" },
-  { images: "flower2.jpg" },
-  { images: "flower3.jpg" },
-  { images: "flower4.jpeg" },
-  { images: "flowr4.avif" },
-];
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number) => {
+  return Math.abs(offset) * velocity;
+};
 
-const Slideshow: React.FC = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+const Slideshow = () => {
+  const [[page, direction], setPage] = useState([0, 0]);
+  const imageIndex = wrap(0, images.length, page);
 
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+  const paginate = (newDirection: number) => {
+    setPage([page + newDirection, newDirection]);
   };
 
   return (
-    <div className="flex gap-80">
-      <button onClick={handlePrev}>Previous</button>
-      <motion.div
-        key={currentIndex}
-        initial={{ opacity: 0, y: 200 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -200 }}
-        transition={{ duration: 0.5 }}
-      >
-        <img
-          key={images[currentIndex].images}
-          src={images[currentIndex].images}
-          alt={`flower-${currentIndex}`}
+    <>
+      <AnimatePresence initial={false} custom={direction}>
+        <motion.img
+          key={page}
+          src={images[imageIndex]}
+          custom={direction}
+          variants={variants}
+          initial="enter"
+          animate="center"
+          exit="exit"
+          transition={{
+            x: { type: "spring", stiffness: 300, damping: 30 },
+            opacity: { duration: 0.2 }
+          }}
+          drag="x"
+          dragConstraints={{ left: 0, right: 0 }}
+          dragElastic={1}
+          onDragEnd={(e, { offset, velocity }) => {
+            const swipe = swipePower(offset.x, velocity.x);
+
+            if (swipe < -swipeConfidenceThreshold) {
+              paginate(1);
+            } else if (swipe > swipeConfidenceThreshold) {
+              paginate(-1);
+            }
+          }}
         />
-      </motion.div>
-      <button onClick={handleNext}>Next</button>
-    </div>
+      </AnimatePresence>
+      <div className={styles.next} onClick={() => paginate(1)}>
+        {"‣"}
+      </div>
+      <div className={styles.prev} onClick={() => paginate(-1)}>
+        {"‣"}
+      </div>
+    </>
   );
 };
-
 export default Slideshow;
